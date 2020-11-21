2958794ede9f252b15afa2a701aa3873839c8c2f
YunhoKim21 <41912350+YunhoKim21@users.noreply.github.com>
2020-11-21 22:49:03 +0900

    display update

--- display.py
+++ display.py
@@ -11,7 +11,7 @@ WHITE= (255,255,255)
 BLUE = ( 0,  0,255)
 GREEN= ( 0,255,  0)
 RED  = (255,  0,  0)
- 
+GREY = (100, 100, 100)
 # Set the height and width of the screen
 size  = [800,600]
 width = size[0]
@@ -20,7 +20,7 @@ height = size[1]
 #game size etc
 outerradius = 250
 innerradius = 230
-
+blockwidth = int(outerradius/sizeofgame)
 screen= pygame.display.set_mode(size)
   
 pygame.display.set_caption("the Gerrymandering")
@@ -45,7 +45,13 @@ while not done:
 
     for i in range(2*sizeofgame):
         for j in range(2*sizeofgame):
-            pass
+            party = game.board[i][j]
+            color = GREY
+            if party == 0: #rep
+                color = RED
+            if party == 1:
+                color = BLUE
+            pygame.draw.rect(screen, color, [width/2-outerradius+i*blockwidth,height/2-outerradius+j*blockwidth , blockwidth, blockwidth])
     
     pygame.display.flip()
 
